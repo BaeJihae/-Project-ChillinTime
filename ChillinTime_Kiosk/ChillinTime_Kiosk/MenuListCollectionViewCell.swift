a592a332dbe8fd211b49f56ea26f161582bd06e3
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-04-03 02:10:20 +0900

    [지해] MenuList 구현

---
+++ ChillinTime_Kiosk/ChillinTime_Kiosk/MenuListCollectionViewCell.swift
@@ -0,0 +1,60 @@
+//
+//  MenuListCollectionViewCell.swift
+//  ChillinTime_Kiosk
+//
+//  Created by 배지해 on 4/3/24.
+//
+
+import UIKit
+import Foundation
+
+class MenuListCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var menuImageView: UIImageView!
+    @IBOutlet weak var menuPriceLabel: UILabel!
+    @IBOutlet weak var menuNameLabel: UILabel!
+    
+    
+    // cell 선택시 색깔 변경
+    override var isHighlighted: Bool {
+        didSet {
+            guard self.isHighlighted else { return }
+            
+            contentView.backgroundColor = UIColor(red: 128/255, green: 202/255, blue: 255/255, alpha: 0.3)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
+                self.contentView.backgroundColor = .white
+            }
+        }
+    }
+    
+    
+    func setConfigue(_ data: MenuData){
+        
+        
+        // 이미지가 있다면 설정
+        if let image = data.image {
+            menuImageView.image = image
+        }
+        
+        
+        // 메뉴 가격 설정
+        menuPriceLabel.text = String(data.price) + "원"
+        
+        
+        // 메뉴 이름 설정
+        menuNameLabel.text = data.name
+        
+        
+        // 메뉴 cell 테두리 설정
+        contentView.layer.cornerRadius = 16
+        contentView.clipsToBounds = true
+        contentView.layer.borderWidth = 1
+        contentView.layer.borderColor = UIColor(red: 128/255, green: 202/255, blue: 255/255, alpha: 1.0).cgColor
+    }
+    
+    
+    
+    
+}
--- ChillinTime_Kiosk/ChillinTime_Kiosk/ViewController.swift
+++ ChillinTime_Kiosk/ChillinTime_Kiosk/ViewController.swift
@@ -33,6 +33,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         setBackColor()
+        setCollectionView()
     }
 
     
@@ -116,3 +117,63 @@ class ViewController: UIViewController {
     
 }
 
+
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    // collectionView setting
+    func setCollectionView() {
+        menuCollectionView.dataSource = self
+        menuCollectionView.delegate = self
+    }
+    
+    
+    // collectionView cell 개수
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return menuData.count
+    }
+    
+    
+    // collectionView와 cell 연결 / menuData 전달
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MenuCell", for: indexPath) as? MenuListCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.setConfigue(menuData[indexPath.row])
+        
+        return cell
+    }
+
+    
+    // collectionView 선택시 셀 배경색 변경 / cart에 추가
+    func collectionView(_ collectionView: UICollectionView,
+                        didSelectItemAt indexPath: IndexPath) {
+        cartDataManager.addCartData(name: menuData[indexPath.row].name)
+    }
+    
+    
+    // collectionView의 view 사이즈 설정
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 170, height: 170)
+    }
+    
+    
+    // collectionView의 행 간격 설정
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 17
+    }
+    
+    
+    
+}
