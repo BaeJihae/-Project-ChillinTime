53e3c158393bffd4b5d365d501bb7c3b4e0aed31
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-04-03 01:06:04 +0900

    프로젝트 생성

---
+++ ChillinTime_Kiosk/ChillinTime_Kiosk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChillinTime_Kiosk
+//
+//  Created by 배지해 on 4/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
